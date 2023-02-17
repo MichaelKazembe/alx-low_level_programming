c79df52e064d583e10f6fe1f3e7aad418e85aa4f
MichaelKazembe <michaelkazembe@gmail.com>
2023-02-17 12:20:49 -0800

    removed spaces

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,6 +6,7 @@
 /**
  * main - Entry point
  *
+ *
  * Return: Always return 0
  *
  */
@@ -20,16 +21,15 @@ int main(void)
 	Last_digit = n % 10;
 
 
-	if ( Last_digit > 5)
+	if (Last_digit>5)
 	{
-		printf("Last digit of %d is %d is greater than 5\n", n , Last_digit);
-	} else if ( Last_digit == 0)
+		printf("Last digit of %d is %d is greater than 5\n", n, Last_digit);
+	} else if (Last_digit==0)
 	{
 		printf("Last digit of %d is %d is 0\n", n, Last_digit);
 	} else
 	{
 		printf("Last digit of %d is %d is and is less than 6 and not 0\n", n, Last_digit);
 	}
-
 	return (0);
 }
