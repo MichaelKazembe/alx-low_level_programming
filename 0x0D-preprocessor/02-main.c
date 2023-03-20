3f68a3634ae3ba04e9f546758bad4ab1c3767f6e
MichaelKazembe <michaelkazembe@gmail.com>
2023-03-20 03:29:55 -0700

    all *.c files

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints name of file it is complied from
+ *
+ * return: Always 0
+ */
+
+int main()
+{
+	printf("The Name of the file is : %s\n", __FILE__);
+
+	return (0);
+}
+
