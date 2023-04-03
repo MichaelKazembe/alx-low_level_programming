a7e7353b22f09b5b73536e6672567d650d295af4
MichaelKazembe <michaelkazembe@gmail.com>
2023-04-03 15:49:10 -0700

    understandable variables

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,15 +9,15 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t count;
+	size_t elements;
 
-	count = 0;
+	elements = 0;
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		count++;
+		elements++;
 	}
-	return (count);
+	return (elements);
 
 }
