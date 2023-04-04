10e1f1cca7545c726df00349d840056b047c39df
MichaelKazembe <michaelkazembe@gmail.com>
2023-04-04 11:52:06 -0700

    betty compliant

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,14 +18,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new_node == NULL)
 	{
 		return (NULL);
-	}	
+	}
 	new_node->n = n;
-	new_node->next = NULL; 
+	new_node->next = NULL;
 	while (idx != 0)
 	{
 		head = &((*head)->next);
 		idx--;
-		
 	}
 
 	new_node->next = *head;
