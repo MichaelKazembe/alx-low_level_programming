d10fed41850f27b3e21f0dff338a3fe382a42f84
MichaelKazembe <michaelkazembe@gmail.com>
2023-05-18 15:37:43 -0700

    betty coding style

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * insert_dnodeint_at_index - Inserts a new node at a given position in a doubly linked list.
+ * insert_dnodeint_at_index - Inserts a new node at a given position.
  * @h: Pointer to the pointer of the head of the list.
  * @idx: Index of the list where the new node should be added (starts at 0).
  * @n: Value to be stored in the new node.
@@ -13,14 +13,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int count = 0;
 
 	if (h == NULL)
-	{
 		return (NULL);
-	}
+
 	newNode = malloc(sizeof(dlistint_t));
 	if (newNode == NULL)
-	{
 		return (NULL);
-	}
 	newNode->n = n;
 	newNode->prev = NULL;
 	newNode->next = NULL;
@@ -29,27 +26,21 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		newNode->next = *h;
 		if (*h != NULL)
-		{
 			(*h)->prev = newNode;
-		}
 		*h = newNode;
 		return (NULL);
 	}
-
 	current = *h;
-
 	while (current != NULL && count < idx - 1)
 	{
 		current = current->next;
 		count++;
 	}
-
 	if (current == NULL)
 	{
 		free(newNode);
 		return (NULL);
 	}
-
 	newNode->next = current->next;
 	if (current->next != NULL)
 	{
@@ -57,6 +48,5 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	current->next = newNode;
 	newNode->prev = current;
-
 	return (newNode);
 }
