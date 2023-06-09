b243e3d0a661d3b3f54cf8dae451986793a9d6b0
MichaelKazembe <michaelkazembe@gmail.com>
2023-06-09 10:49:34 -0700

    requirement

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -16,7 +16,7 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 		return (0);
 	}
 
-	index = hash_djb2(key) % size;
+	index = ((hash_djb2(key)) % size);
 
 	return (index);
 }
