83942e9a71f399332c73dd6a931390d33b179469
MichaelKazembe <michaelkazembe@gmail.com>
2023-06-09 12:52:19 -0700

    print hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,39 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - function that retrieves a value associated with a key.
+ * @ht: the hash table
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	long unsigned int i;
+	hash_node_t *node;
+	int is_first = 1;
+
+	if (ht == NULL)
+	{
+		return;
+	}
+
+	printf("{");
+
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			if (!is_first)
+			{
+				printf(", ");
+			}
+			printf("'%s': '%s'", node->key, node->value);
+			is_first = 0;
+			node = node->next;
+		}
+	}
+
+	printf("}\n");
+
+}
+
