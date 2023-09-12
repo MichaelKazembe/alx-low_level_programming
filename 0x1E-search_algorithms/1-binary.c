6baa2ea53bb6708326e452e3cc0d3f3a122db335
Michael Kazembe <122834780+MichaelKazembe@users.noreply.github.com>
2023-09-12 12:46:36 +0200

    Update 1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,51 +1,46 @@
 #include "search_algos.h"
-/**
- * print_array - print values of actual array
- * @array: array given as parameter
- * @left: left index of the array
- * @right: right index of the array
- * Return: void
- */
-void print_array(int *array, int left, int right)
-{
 
-	while (left <= right)
-	{
-		if (left < right)
-			printf("%d, ", array[left]);
-		else
-			printf("%d", array[left]);
-		left = left + 1;
-	}
-	printf("\n");
-}
 /**
- * binary_search - performs binary search over a given array
- * @array: given array
- * @size: array size
- * @value: value to search
- * Return: index of value if it's found otherwise -1
+ * binary_search - Searches for a value in a sorted array using binary search
+ * @array: Pointer to the first element of the array
+ * @size: The number of elements in the array
+ * @value: The value to search for
+ *
+ * Return: The index of the value if found, or -1 if not found or array is NULL
  */
+
 int binary_search(int *array, size_t size, int value)
 {
-	int begin, end, middle;
+	int left = 0;
+	int mid;
+	int right = size - 1;
+	int i;
 
 	if (array == NULL)
 		return (-1);
 
-	begin = 0;
-	end = (int)size - 1;
-	while (begin <= end)
+	while (left <= right)
 	{
 		printf("Searching in array: ");
-		print_array(array, begin, end);
-		middle = (double)((begin + end) / 2);
-		if (array[middle] == value)
-			return ((int)middle);
-		else if (value > array[middle])
-			begin = middle + 1;
-		else
-			end = middle - 1;
+		for (i = left; i <= right; i++)
+		{
+			printf("%d", array[i]);
+			if (i < right)
+				printf(", ");
+		}
+		printf("\n");
+
+	mid = left + (right - left) / 2;
+
+	if (array[mid] == value)
+		return (mid);
+
+	else if (value < array[mid])
+		right = mid - 1;
+
+	else
+		left = mid + 1;
 	}
+
 	return (-1);
 }
