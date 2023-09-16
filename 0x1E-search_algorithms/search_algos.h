4cd72af64628dfbf6cd1af71f44ae0ecf35b9f2d
MichaelKazembe <michaelkazembe@gmail.com>
2023-09-16 12:29:27 -0700

    time complexity fot Jump search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,11 +3,15 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 /* Linear Search */
 int linear_search(int *array, size_t size, int value);
 
 /* Binary Search */
 int binary_search(int *array, size_t size, int value);
+
+/* Jump Search */
+int jump_search(int *array, size_t size, int value);
 
 #endif
