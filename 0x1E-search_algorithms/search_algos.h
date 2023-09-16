61c9ab18dab2b706cb4a0b0c7f56323b9afff984
MichaelKazembe <michaelkazembe@gmail.com>
2023-09-16 14:51:54 -0700

    103-exponential

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -14,4 +14,10 @@ int binary_search(int *array, size_t size, int value);
 /* Jump Search */
 int jump_search(int *array, size_t size, int value);
 
+/* Interpolation Search */
+int interpolation_search(int *array, size_t size, int value);
+
+/* Exponential Search */
+int exponential_search(int *array, size_t size, int value);
+
 #endif
